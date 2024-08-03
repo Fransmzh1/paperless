68f5c6c4eedebff7ec28465e305d9d7342dd9584
Frans <frans.mazhar@gmail.com>
2024-08-03 21:28:17 +0700

    udpate

--- summary-svc/summary.py
+++ summary-svc/summary.py
@@ -1,28 +1,34 @@
-import requests
+import os
 
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 def call_dms():
     """
     Calls an external service using the POST method.
 
-    :param url: The URL of the external service.
+    :param url: The URL of the external servisce.
     :param data: The data to be sent in the POST request (typically a dictionary).
     :param headers: Optional headers to include in the request.
     :return: Response object from the request.
     """
 
     url = "https://dms.isinovasi.co.id/api/documents/9/"
-    headers = {"Authorization": "Basic ZnJhbnM6bGlidXJhbnNlcnU="}
+    dms_cred = os.environ["DMS_CRED"]
+    headers = {"Authorization": dms_cred}
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
         return response.json()  # Return the JSON response (or use response.text if not JSON)
     except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+        print("An error occurred: {e}")
         return None
 
 def call_summarize(data):
     url = "https://api.perplexity.ai/chat/completions"
-    apikey = "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"
+    apikey = os.environ["API_KEY"]
     payload = {
         "model": "llama-3.1-8b-instruct",
         "messages": [
